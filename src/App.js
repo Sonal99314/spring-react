04383387eeb8553be2f0a4eb014ea720696d8266
Your Name <you@example.com>
2022-02-17 13:53:21 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './components/login';
+import SignInOutContainer from './containers.jsx';
 function App() {
   return (
     <div className="App">
-      <Login />
+       <SignInOutContainer/>
+      
     </div>
   );
 }
